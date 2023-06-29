61dfa8a572a8425dbf396301df6bc5a952cf63cf
Alvarobyui <u701860138@byui.edu>
2023-06-29 14:06:53 -0500

    first banner added

---
+++ app/components/Banner-Story.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import "./banner-Story.css"
+
+export default function BannerStory() {
+  return (
+    <div className="banner-container">
+      <div className="title">
+        <p>Unhappy with your website?</p>
+        <h1>We create beautiful and fast web services</h1>
+      </div>
+      <div id="banner">
+        <img src="/heroImage.jpg" alt="img" />
+      </div>
+
+      <div className="story">
+        <h1>Story, emotion and purpose</h1>
+        <p>
+          We may help transform your ideas into real world applications that
+          will outperform your toughest competition and help you achieve your
+          strategic goals in short period of time.
+        </p>
+
+        <div className="input-box">
+          <p>Want us to contact you?</p>
+          <input type="text" placeholder="Email" />
+          <button type="submit">Join</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import Nav from "./components/Nav"
+import BannerAndStory from "./components/Banner-Story";
 
 export default function Home() {
   return (
     <div className="container">
       <Nav />
+      <BannerAndStory />
+      <h1>We offer high demand services</h1>
     </div>
   )
 }
